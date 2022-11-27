29873c16caa3b0bb9ffa4175a3f6ab5c32b92de3
suprajak <suprajakolla23@gmail.com>
2022-11-27 23:46:01 +0530

    java

---
+++ src/main/java/org/example/Employee.java
@@ -0,0 +1,32 @@
+package org.example;
+
+public class Employee {
+    String empName;
+
+    double sal;
+    double netsal;
+
+    public String getEmpName() {
+        return empName;
+    }
+
+    public void setEmpName(String empName) {
+        this.empName = empName;
+    }
+
+    public double getSal() {
+        return sal;
+    }
+
+    public void setSal(double sal) {
+        this.sal = sal;
+    }
+
+    public double getNetsal() {
+        return netsal;
+    }
+
+    public void setNetsal(double netsal) {
+        this.netsal = netsal;
+    }
+}
